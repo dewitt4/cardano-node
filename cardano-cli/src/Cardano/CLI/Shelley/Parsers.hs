817650dc035b5aa13a00d4c3cde2859ec36bb718
Jordan Millar <jordan.millar@iohk.io>
2021-04-08 11:14:04 +0100

    Prepare Cardano.Api.ProtocolParameters for Alonzo era. In the Alonzo era we introduce 6 new protocol parameters and remove 1 protocol parameter. In anticipation of the Alonzo era, we make some modifications to the ProtocolParameters type.
    minUTxOValue becomes a Maybe (This parameter gets deprecated in Alonzo)
    
    We introduce the following but note they are currently ignored:
    - adaPerUTxoByte - Cost in ada per byte of UTxO storage (replaces
    minUTxOValue)
    - costmdls - The cost models for plutus scripts
    - prices - The prices of execution units for plutus scripts
    - maxTxExUnits - Max total script execution resource  units allowed per tx
    - maxBlockExUnits - Max total script execution resource units allowed per block
    - maxValSize - Max size of a Value in a tx output
    
    We introduce an era parameter in the ProtocolParameters type in order to
    reduce boilerplate. We have avoided the typical GADT pattern in
    cardano-api to prevent an explosion of types.

--- cardano-cli/src/Cardano/CLI/Shelley/Parsers.hs
+++ cardano-cli/src/Cardano/CLI/Shelley/Parsers.hs
@@ -561,8 +561,8 @@ pTransaction =
 
   pTransactionCalculateMinValue :: Parser TransactionCmd
   pTransactionCalculateMinValue = TxCalculateMinValue
-    <$> pProtocolParamsSourceSpec
-    <*> pMultiAsset
+    <$> (ParamsFromFile <$> pProtocolParamsFile)
+    <*> pTxBodyFile Input
 
   pProtocolParamsSourceSpec :: Parser ProtocolParamsSourceSpec
   pProtocolParamsSourceSpec =
@@ -1628,15 +1628,6 @@ pTxOut =
                   \Lovelace."
       )
 
-pMultiAsset :: Parser Value
-pMultiAsset =
-  Opt.option
-    (readerFromParsecParser parseValue)
-      (  Opt.long "multi-asset"
-      <> Opt.metavar "VALUE"
-      <> Opt.help "Multi-asset value(s) with the multi-asset cli syntax"
-      )
-
 pMintMultiAsset :: Parser (Value, [ScriptFile])
 pMintMultiAsset =
   (,) <$> Opt.option
@@ -2219,6 +2210,15 @@ pShelleyProtocolParametersUpdate =
     <*> optional pPoolInfluence
     <*> optional pMonetaryExpansion
     <*> optional pTreasuryExpansion
+    -- TODO: Update when Alonzo is incorporated
+    <*> pure Nothing
+    <*> pure Nothing
+    <*> pure Nothing
+    <*> pure Nothing
+    <*> pure Nothing
+    <*> pure Nothing
+
+
 
 pMinFeeLinearFactor :: Parser Natural
 pMinFeeLinearFactor =
